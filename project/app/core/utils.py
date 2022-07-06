7f4c0fe0fefd7e644a5ecc684e6a585786c5abe7
Fersoftware <fersoftware@gmail.com>
2022-07-06 17:04:28 -0300

    fix task

--- project/app/core/utils.py
+++ project/app/core/utils.py
@@ -15,6 +15,7 @@ def gravabanco_ctrl_arq_excel(dados):
     except Exception as e:
       raise e
 
+
 def dif_month(data1, data2):    
     ano, mes, dia = data1.split('-')
     d1 = datetime(int(ano),int(mes),int(dia))    
