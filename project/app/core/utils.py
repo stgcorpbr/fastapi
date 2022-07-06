753800bb246222fc59a488b7fe780903cdb84faf
Fersoftware <fersoftware@gmail.com>
2022-07-06 17:04:00 -0300

    fix task

--- project/app/core/utils.py
+++ project/app/core/utils.py
@@ -17,9 +17,9 @@ def gravabanco_ctrl_arq_excel(dados):
 
 def dif_month(data1, data2):    
     ano, mes, dia = data1.split('-')
-    d1 = datetime.datetime(int(ano),int(mes),int(dia))    
+    d1 = datetime(int(ano),int(mes),int(dia))    
     ano, mes, dia = data2.split('-')
-    d2 = datetime.datetime(int(ano),int(mes),int(dia))
+    d2 = datetime(int(ano),int(mes),int(dia))
     return (d2 - d1).days // 30
 
 def ren(dict, novo, antigo):
