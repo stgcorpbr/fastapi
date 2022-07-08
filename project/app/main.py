8c08fdcd8dd3f732a5933cfd7835b1d31db76620
Fersoftware <fersoftware@gmail.com>
2022-07-08 15:44:12 -0300

    fix task

--- project/app/main.py
+++ project/app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET","DELETE", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
